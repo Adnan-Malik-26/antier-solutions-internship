6fdd63a78f731c82e870d7b993b12b7e81809ef8
Adnan-Malik-26 <eddymalikadnan@gmail.com>
2024-07-18 15:46:49 +0530

    Added Lottery.sol

--- solidity-programs/lottery.sol
+++ solidity-programs/lottery.sol
@@ -6,15 +6,30 @@ contract Lottery {
         string name;
         uint ticketNumber;
     }
+    uint winningNumber;
 
-    mapping(uint => Ticket) public tickets;
-
+    mapping(uint => string) public tickets;
+    
+    
     function buyTicket(string memory name, uint ticketNumber) public returns (bool) {
-        if (tickets[ticketNumber].ticketNumber != 0) {
+        if (keccak256(abi.encodePacked(tickets[ticketNumber])) ==  keccak256(abi.encodePacked(""))) {
+            tickets[ticketNumber]=name;
+            return true;
+        }
+
+        return false;
+    }
+
+    function setWinningNumber(string memory name, uint ticketNumber) public returns (bool){
+        if(keccak256(abi.encodePacked(name)) != keccak256(abi.encodePacked("admin"))){
             return false;
         }
 
-        tickets[ticketNumber] = Ticket(name, ticketNumber);
+        winningNumber=ticketNumber;
         return true;
+    }
+
+    function returnWinningNumber()public view returns(uint){
+        return winningNumber;
     }
 }
