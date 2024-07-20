a4889f3fd62ac46335c66c2e43c0a4da987cc5d1
Adnan-Malik-26 <eddymalikadnan@gmail.com>
2024-07-20 21:17:31 +0530

    changed lottery.sol

--- solidity-programs/lottery.sol
+++ solidity-programs/lottery.sol
@@ -6,18 +6,30 @@ contract Lottery {
         string name;
         uint ticketNumber;
     }
+    
+    Ticket[] public dataArray;
+
     uint winningNumber;
 
+    address user = msg.sender;    
+
     mapping(uint => string) public tickets;
-    
+    mapping(uint=>Ticket) public numToData;
+
+    event DataStored(string name, uint Amount);
     
     function buyTicket(string memory name, uint ticketNumber) public returns (bool) {
         if (keccak256(abi.encodePacked(tickets[ticketNumber])) ==  keccak256(abi.encodePacked(""))) {
             tickets[ticketNumber]=name;
-            return true;
+            emit DataStored(name,ticketNumber);
         }
-
         return false;
+    }
+
+    function buyTicket1( string memory name, uint ticketNumber) public {
+        require((keccak256(abi.encodePacked(tickets[ticketNumber])) ==  keccak256(abi.encodePacked(""))), "Ticket Already Exists");
+        numToData[ticketNumber] = Ticket(name,ticketNumber);
+        emit DataStored(name,ticketNumber);
     }
 
     function setWinningNumber(string memory name, uint ticketNumber) public returns (bool){
