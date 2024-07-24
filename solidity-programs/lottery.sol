b8c72f3ed14478f9c816e11999566eb39de7d13c
Adnan-Malik-26 <eddymalikadnan@gmail.com>
2024-07-24 23:29:00 +0530

    Added money into the equation

--- solidity-programs/lottery.sol
+++ solidity-programs/lottery.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.21;
 
-contract Lottery {
+contract Lottery{
     struct Ticket {
         address owner;
         uint ticketNumber;
     }
+    uint randNum;
+    uint constant ticketPrice = 100000000 gwei;
     
     Ticket[] public dataArray;
 
@@ -16,39 +18,61 @@ contract Lottery {
 
     event TicketBought(address indexed owner, uint ticketNumber);
     event WinningNumberSet(uint winningNumber);
+    address owner = msg.sender;
+
+    modifier onlyOwner(){
+        require(owner==msg.sender,"You are not the Owner");
+        _;
+    }
 
     constructor() {
         ticketsExist[0] = true;
     }
 
-    function buyTicket(uint ticketNumber) public returns (bool) {
+    function buyTicket(uint ticketNumber) public payable returns (bool) {
         require(!ticketsExist[ticketNumber], "Ticket already bought.");
         require(addressToTicketNumber[msg.sender] == 0, "You have already bought a ticket.");
+        require(msg.value == ticketPrice, "Incorrect ticket price. Please send 100000000 gwei");
 
-        address owner = msg.sender;
         Ticket memory newTicket = Ticket({
-            owner: owner,
+            owner: msg.sender,
             ticketNumber: ticketNumber
         });
 
         dataArray.push(newTicket);
-        addressToTicketNumber[owner] = ticketNumber;
+        addressToTicketNumber[msg.sender] = ticketNumber;
         ticketsExist[ticketNumber] = true;
 
-        emit TicketBought(owner, ticketNumber);
+        emit TicketBought(msg.sender, ticketNumber);
 
         return true;
     }
 
-    function setWinningNumber(uint _winningNumber) public {
-        require(msg.sender == 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, "Only admin can set winning number.");
-
-        winningNumber = _winningNumber;
-
-        emit WinningNumberSet(winningNumber);
+    function setWinningNumber(string memory _anystring) public onlyOwner{
+        winningNumber = uint(keccak256(abi.encodePacked(_anystring)));
+        winningNumber = winningNumber%100;
     }
 
-    function getWinningNumber() public view returns (uint) {
+    function getWinningNumber() public onlyOwner view returns(uint){
         return winningNumber;
     }
-}
+
+    function distributePrize() public onlyOwner{
+        require(winningNumber != 0, "Winning number has not been set yet.");
+
+        address winner = address(0);
+        for (uint i = 0; i < dataArray.length; i++) {
+            if (dataArray[i].ticketNumber == winningNumber) {
+                winner = dataArray[i].owner;
+                break;
+            }
+        }
+
+        require(winner != address(0), "No winner found for the winning number.");
+
+        uint prizeAmount = address(this).balance;
+        payable(winner).transfer(prizeAmount);
+
+    }
+    
+} 
